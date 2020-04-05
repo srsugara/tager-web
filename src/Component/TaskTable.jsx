14b13bcf4b9bd708fae48b741f6f45c20d8a6d91
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-04-05 19:38:31 +0700

    [feat]:filter data from db to show in card

--- src/Component/TaskTable.jsx
+++ src/Component/TaskTable.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import BaseComponent from '../BaseComponent'
+import BaseComponent from '../BaseComponent';
 import TaskCard from './TaskCard';
-import todosStore from '../store/todos'
-import userStore from '../store/user'
+import todosStore from '../store/todos';
+import userStore from '../store/user';
 
 class TaskTable extends BaseComponent {
   componentDidMount() {
@@ -13,107 +13,100 @@ class TaskTable extends BaseComponent {
   componentWillUnmount() {
     this.unsubTodos();
   }
+
+  renderCard(status) {
+    const data = todosStore.data.filter(key => key.status === status);
+    return data.map(function (todo, index) {
+      return <TaskCard key={index} data={todo}/>
+    })
+  }
+
   render() {
     return (
       <div>
         <p>
-          halo {userStore.data.email} <button onClick={this.logout}>logout</button>
+          halo {userStore.data.email}{' '}
+          <button onClick={this.logout}>logout</button>
         </p>
 
         <h2>
-          todos: <button onClick={this.upload}>
+          todos:{' '}
+          <button onClick={this.upload}>
             {`upload (${todosStore.countUnuploadeds()})`}
           </button>
         </h2>
-        <pre>
-          last upload: {todosStore.dataMeta.tsUpload}
-        </pre>
-        {
-          todosStore.data.map((todo, index) => (
-            <p key={todo._id}>
-              {index + 1}. {todo.title}
-              {
-                !todosStore.checkIsUploaded(todo) && (
-                  ` (belum upload)`
-                )
-              }
-              {` `}
-              <button onClick={() => this.deleteTodo(todo._id)}>
-                X
-              </button>
-            </p>
-          ))
-        }
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-black">
-            <div className="content">
-              <p className="title">Add Task</p>
-              <p className="subtitle">With even more content</p>
+        <pre>last upload: {todosStore.dataMeta.tsUpload}</pre>
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-black">
               <div className="content">
-                <div className="field">
+                <p className="title">Add Task</p>
+                <p className="subtitle">With even more content</p>
+                <div className="content">
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Title"
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Description"
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input className="input" type="text" placeholder="Tags" />
+                    </div>
+                  </div>
                   <div className="control">
-                    <input className="input" type="text" placeholder="Title" />
+                    <button className="button is-primary">Submit</button>
                   </div>
                 </div>
-                <div className="field">
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="Description"
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <div className="control">
-                    <input className="input" type="text" placeholder="Tags" />
-                  </div>
-                </div>
-                <div className="control">
-                  <button className="button is-primary">Submit</button>
+              </div>
+            </article>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-warning">
+              <div className="content">
+                <p className="title">Unstarted</p>
+                <p className="subtitle">With even more content</p>
+                <div className="content">
+                 { this.renderCard('unstarted') }
                 </div>
               </div>
-            </div>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-warning">
-            <div className="content">
-              <p className="title">Unstarted</p>
-              <p className="subtitle">With even more content</p>
+            </article>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-info">
               <div className="content">
-                <TaskCard />
-                <TaskCard />
+                <p className="title">Started</p>
+                <p className="subtitle">With even more content</p>
+                <div className="content">
+                { this.renderCard('started') }
+                </div>
               </div>
-            </div>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-info">
-            <div className="content">
-              <p className="title">Started</p>
-              <p className="subtitle">With even more content</p>
+            </article>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-success">
               <div className="content">
-                <TaskCard />
-                <TaskCard />
+                <p className="title">Finished</p>
+                <p className="subtitle">With even more content</p>
+                <div className="content">
+                { this.renderCard('finished') }
+                </div>
               </div>
-            </div>
-          </article>
+            </article>
+          </div>
         </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-success">
-            <div className="content">
-              <p className="title">Finished</p>
-              <p className="subtitle">With even more content</p>
-              <div className="content">
-                <TaskCard />
-                <TaskCard />
-              </div>
-            </div>
-          </article>
-        </div>
-      </div>
       </div>
     );
   }
