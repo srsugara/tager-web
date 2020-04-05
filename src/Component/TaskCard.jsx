37d82b6f9b2c3bbaf819409e052ba8a2cb328d41
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-04-05 13:51:04 +0700

    [style]:add button status task

--- src/Component/TaskCard.jsx
+++ src/Component/TaskCard.jsx
@@ -5,22 +5,26 @@ class TaskCard extends PureComponent {
     return (
       <div class="box">
         <article class="media">
-          <div class="media-left">
-          </div>
+          <div class="media-left"></div>
           <div class="media-content">
             <div class="content">
               <p>
-                <strong>John Smith</strong> <span class="tag is-info is-light">frontend</span>{' '}
+                <strong>John Smith</strong>{' '}
+                <span class="tag is-info is-light">frontend</span>
+                <br/>
                 <small>09:00 02-04-2020</small>
                 <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                efficitur sit amet massa fringilla egestas. Nullam condimentum
-                luctus turpis.
+                <div class="has-text-black">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aenean efficitur sit amet massa fringilla egestas. Nullam
+                  condimentum luctus turpis.
+                </div>
               </p>
             </div>
             <div>
               <button class="button is-primary is-small">Edit</button>{' '}
-              <button class="button is-danger is-small">Delete</button>
+              <button class="button is-danger is-small">Delete</button>{' '}
+              <button class="button is-small">start</button>
             </div>
           </div>
         </article>
