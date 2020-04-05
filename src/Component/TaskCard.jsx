50024440b54ce1e14070d2a5e547d05833feffe9
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-04-05 17:41:59 +0700

    [refactor]:change class to className

--- src/Component/TaskCard.jsx
+++ src/Component/TaskCard.jsx
@@ -3,28 +3,28 @@ import React, { PureComponent } from 'react';
 class TaskCard extends PureComponent {
   render() {
     return (
-      <div class="box">
-        <article class="media">
-          <div class="media-left"></div>
-          <div class="media-content">
-            <div class="content">
-              <p>
+      <div className="box">
+        <article className="media">
+          <div className="media-left"></div>
+          <div className="media-content">
+            <div className="content">
+              <div>
                 <strong>John Smith</strong>{' '}
-                <span class="tag is-info is-light">frontend</span>
+                <span className="tag is-info is-light">frontend</span>
                 <br/>
-                <small class="has-text-black">09:00 02-04-2020</small>
+                <small className="has-text-black">09:00 02-04-2020</small>
                 <br />
-                <div class="has-text-black">
+                <div className="has-text-black">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aenean efficitur sit amet massa fringilla egestas. Nullam
                   condimentum luctus turpis.
                 </div>
-              </p>
+              </div>
             </div>
             <div>
-              <button class="button is-primary is-small">Edit</button>{' '}
-              <button class="button is-danger is-small">Delete</button>{' '}
-              <button class="button is-small">start</button>
+              <button className="button is-primary is-small">Edit</button>{' '}
+              <button className="button is-danger is-small">Delete</button>{' '}
+              <button className="button is-small">start</button>
             </div>
           </div>
         </article>
--- src/Component/TaskTable.jsx
+++ src/Component/TaskTable.jsx
@@ -1,87 +1,119 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
+import BaseComponent from './BaseComponent'
 import TaskCard from './TaskCard';
+import todosStore from '../store/todos'
+import userStore from '../store/user'
 
-class TaskTable extends PureComponent {
+class TaskTable extends BaseComponent {
+  componentDidMount() {
+    this.unsubTodos = todosStore.subscribe(this.rerender);
+  }
+
+  componentWillUnmount() {
+    this.unsubTodos();
+  }
   render() {
     return (
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-black">
-            <div class="content">
-              <p class="title">Add Task</p>
-              <p class="subtitle">With even more content</p>
-              <div class="content">
-                <div class="field">
-                  <div class="control">
-                    <input
-                      class="input"
-                      type="text"
-                      placeholder="Title"
-                    />
+      <div>
+        <p>
+          halo {userStore.data.email} <button onClick={this.logout}>logout</button>
+        </p>
+
+        <h2>
+          todos: <button onClick={this.upload}>
+            {`upload (${todosStore.countUnuploadeds()})`}
+          </button>
+        </h2>
+        <pre>
+          last upload: {todosStore.dataMeta.tsUpload}
+        </pre>
+        {
+          todosStore.data.map((todo, index) => (
+            <p key={todo._id}>
+              {index + 1}. {todo.title}
+              {
+                !todosStore.checkIsUploaded(todo) && (
+                  ` (belum upload)`
+                )
+              }
+              {` `}
+              <button onClick={() => this.deleteTodo(todo._id)}>
+                X
+              </button>
+            </p>
+          ))
+        }
+      <div className="tile is-ancestor">
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-black">
+            <div className="content">
+              <p className="title">Add Task</p>
+              <p className="subtitle">With even more content</p>
+              <div className="content">
+                <div className="field">
+                  <div className="control">
+                    <input className="input" type="text" placeholder="Title" />
                   </div>
                 </div>
-                <div class="field">
-                  <div class="control">
+                <div className="field">
+                  <div className="control">
                     <input
-                      class="input"
+                      className="input"
                       type="text"
                       placeholder="Description"
                     />
                   </div>
                 </div>
-                <div class="field">
-                  <div class="control">
-                    <input
-                      class="input"
-                      type="text"
-                      placeholder="Tags"
-                    />
+                <div className="field">
+                  <div className="control">
+                    <input className="input" type="text" placeholder="Tags" />
                   </div>
                 </div>
-                <div class="control">
-                  <button class="button is-primary">Submit</button>
+                <div className="control">
+                  <button className="button is-primary">Submit</button>
                 </div>
               </div>
             </div>
           </article>
         </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-warning">
-            <div class="content">
-              <p class="title">Unstarted</p>
-              <p class="subtitle">With even more content</p>
-              <div class="content">
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-warning">
+            <div className="content">
+              <p className="title">Unstarted</p>
+              <p className="subtitle">With even more content</p>
+              <div className="content">
                 <TaskCard />
                 <TaskCard />
               </div>
             </div>
           </article>
         </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-info">
-            <div class="content">
-              <p class="title">Started</p>
-              <p class="subtitle">With even more content</p>
-              <div class="content">
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-info">
+            <div className="content">
+              <p className="title">Started</p>
+              <p className="subtitle">With even more content</p>
+              <div className="content">
                 <TaskCard />
                 <TaskCard />
               </div>
             </div>
           </article>
         </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-success">
-            <div class="content">
-              <p class="title">Finished</p>
-              <p class="subtitle">With even more content</p>
-              <div class="content">
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-success">
+            <div className="content">
+              <p className="title">Finished</p>
+              <p className="subtitle">With even more content</p>
+              <div className="content">
                 <TaskCard />
                 <TaskCard />
               </div>
             </div>
           </article>
         </div>
+      </div>
       </div>
     );
   }
