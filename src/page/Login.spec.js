509a2ebe1626982cf37818dca9bf7b109fec6dff
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-05-03 21:17:02 +0700

    feat(component):add script for unit test

---
+++ src/page/Login.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// import { render } from '@testing-library/react';
+import Login from './Login';
+
+// 1. Menampilkan label button yang benar. yaitu: Login
+it('shows correct header', () => {
+  const wrapper = shallow(<Login />);
+  expect(wrapper.find('.button.is-primary.is-fullwidth').text()).toBe('Login');
+});
+
+it('check email state from input', () => {
+  const wrapper = shallow(<Login />);
+  wrapper
+    .find('input[type="email"]')
+    .simulate('change', { target: { value: 'syauqi@gmail.com' } });
+  expect(wrapper.state('email')).toEqual('syauqi@gmail.com');
+});
+
+// it('login check with right data format', () => {
+//   const wrapper = shallow(<Login />);
+//   wrapper
+//     .find('input[type="email"]')
+//     .simulate('change', { target: { value: 'syauqi@gmail.com' } });
+//   wrapper.find('button').simulate('click');
+//   expect(wrapper.state('email')).toEqual('syauqi@gmail.com');
+// });
