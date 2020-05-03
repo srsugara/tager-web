fbdf30558612866711ed83655c6fef463b3957b3
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-05-03 21:15:16 +0700

    refactor(component):separate component to directory

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import userStore from './store/user';
 import config from './config';
 
 import BaseComponent from './BaseComponent';
-import TaskTable from './component/TaskTable';
+import Login from './page/Login'
+import TaskTable from './components/TaskTable';
 
 // for playin in browser console
 window.userStore = userStore;
@@ -55,82 +56,6 @@ class App extends BaseComponent {
 
   componentWillUnmount() {
     this.unsubUser();
-  }
-}
-
-class Login extends BaseComponent {
-  state = {
-    email: '',
-  };
-
-  setInputEmail = (event) => {
-    this.setState({
-      email: (event.target.value || '').trim(),
-    });
-  };
-
-  submit = async (event) => {
-    event.preventDefault();
-
-    if (!this.state.email) {
-      alert('Use your @gmail.com account');
-      return;
-    }
-    if (!this.state.email.endsWith('@gmail.com')) {
-      alert('Use your @gmail.com account');
-      return;
-    }
-
-    let id = this.state.email;
-    id = id.split('@').shift().replace(/\W/g, '');
-
-    await userStore.editSingle({
-      id,
-      email: this.state.email,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <section className="hero is-fullheight is-medium is-primary is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <div className="columns is-centered">
-                <article className="card is-rounded">
-                  <br />
-                  <center>
-                    <h1 className="title">
-                      <div className="has-text-black">Tager Login</div>
-                    </h1>
-                  </center>
-                  <div className="card-content">
-                    <p className="control">
-                      <input
-                        className="input"
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.setInputEmail}
-                        placeholder="name@gmail.com"
-                      />
-                    </p>
-                    <br />
-                    <p className="control">
-                      <button
-                        className="button is-primary is-fullwidth"
-                        onClick={this.submit}
-                      >
-                        Login
-                      </button>
-                    </p>
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
   }
 }
 
---
+++ src/page/Login.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import userStore from '../store/user';
+
+import BaseComponent from '../BaseComponent';
+
+class Login extends BaseComponent {
+    state = {
+      email: ''
+    };
+  
+    setInputEmail = (event) => {
+      this.setState({
+        email: (event.target.value || '').trim(),
+      });
+    };
+  
+    submit = async (event) => {
+      event.preventDefault();
+  
+      if (!this.state.email) {
+        alert('Use your @gmail.com account');
+        return;
+      }
+      if (!this.state.email.endsWith('@gmail.com')) {
+        alert('Use your @gmail.com account');
+        return;
+      }
+      
+      let id = this.state.email;
+      id = id.split('@').shift().replace(/\W/g, '');
+  
+      await userStore.editSingle({
+        id,
+        email: this.state.email,
+      });
+    };
+  
+    render() {
+      return (
+        <div>
+          <section className="hero is-fullheight is-medium is-primary is-bold">
+            <div className="hero-body">
+              <div className="container">
+                <div className="columns is-centered">
+                  <article className="card is-rounded">
+                    <br />
+                    <center>
+                      <h1 className="title">
+                        <div className="has-text-black">Tager Login</div>
+                      </h1>
+                    </center>
+                    <div className="card-content">
+                      <p className="control">
+                        <input
+                          className="input"
+                          type="email"
+                          value={this.state.email}
+                          onChange={this.setInputEmail}
+                          placeholder="name@gmail.com"
+                        />
+                      </p>
+                      <br />
+                      <p className="control">
+                        <button
+                          className="button is-primary is-fullwidth"
+                          onClick={this.submit}
+                        >
+                          Login
+                        </button>
+                      </p>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      );
+    }
+  }
+
+  export default Login;
